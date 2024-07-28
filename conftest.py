29737a07e5e7131c08adaef23593b60dda8e65bf
Vera Popova <info@verapo.com>
2024-07-28 21:07:27 +0300

    Add headless mode and other options for Chrome in GitHub Actions

--- conftest.py
+++ conftest.py
@@ -1,4 +1,4 @@
-
+# conftest.py
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -9,18 +9,19 @@ import os
 @pytest.fixture(scope="session")
 def driver():
     chrome_options = Options()
+    chrome_options.add_argument('--headless')  # Запуск в headless режиме
+    chrome_options.add_argument('--no-sandbox')  # Отключение sandbox режима
+    chrome_options.add_argument('--disable-dev-shm-usage')  # Использование shared memory
     chrome_options.add_argument('--ignore-certificate-errors')
     chrome_options.add_argument('--ignore-ssl-errors')
     chrome_options.add_argument('--disable-web-security')
     chrome_options.add_argument('--allow-running-insecure-content')
+    chrome_options.add_argument('--disable-gpu')  # Отключение GPU, не требуется в headless режиме
 
-    # Проверка, что путь к драйверу верен
-    #driver_path = os.path.abspath("./drivers/chromedriver.exe")
-    #service = Service(driver_path)
-
+    # Используем webdriver_manager для автоматической установки chromedriver
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
     driver.set_window_size(1920, 1080)  # Установка разрешения окна на 1920x1080
-    driver.implicitly_wait(1)  # Установка неявного ожидания
+    driver.implicitly_wait(10)  # Установка неявного ожидания
     yield driver
     driver.quit()
