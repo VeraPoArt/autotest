8a6d8d6e00693e17555f19aad9b7ff970b1b1fac
Vera Popova <info@verapo.com>
2024-07-28 21:05:11 +0300

    Fix import for ChromeDriverManager

--- conftest.py
+++ conftest.py
@@ -3,6 +3,7 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 import os
 
 @pytest.fixture(scope="session")
